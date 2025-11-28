090adcd0ab06099d14fa3e91c4d6aafb1ea23f41
kelvyn2012 <kudufo@gmail.com>
2025-11-28 21:07:08 +0100

    Prepare for Render deployment

--- notely/notely/settings.py
+++ notely/notely/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -72,6 +73,7 @@ WSGI_APPLICATION = "notely.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
+DATABASE_PATH = os.getenv('DATABASE_PATH', 'db.sqlite3')
 
 DATABASES = {
     "default": {
